7df8a007579f47b916bc553a450e91d3450731cb
AlexBashorin <bashorina5@gmail.com>
2024-01-16 11:31:54 +0700

    simple form

--- main.go
+++ main.go
@@ -1,73 +1,72 @@
 package main
 
 import (
-	"bufio"
 	"encoding/json"
-	"fmt"
+	"html/template"
 	"io"
-	"math/rand"
+	"log"
+	"net/http"
 	"os"
-	"strings"
 )
 
-type Compare struct {
+type JsonData struct {
 	Eng string `json:"eng"`
 	Fin string `json:"fin"`
 }
+type Domdata struct {
+	PageTitle string
+	EngValue  string
+	Answer    string
+}
+
+var content []JsonData
 
 func main() {
-	// how many words
-	var countWords int
-	fmt.Println("Выбери количество слов на сегодня")
-	_, err := fmt.Scanln(&countWords)
-	if err != nil {
-		fmt.Printf("Введенное значение неверно: %v", err)
-	}
-	fmt.Printf("Количество слов: %d\n", countWords)
+	http.HandleFunc("/", home)
+	http.HandleFunc("/check", func(w http.ResponseWriter, r *http.Request) {
 
-	// read source json
-	fJson, err := os.Open("qa.json")
-	if err != nil {
-		fmt.Printf("didn't open json")
-	}
-	defer fJson.Close()
-
-	byteValue, _ := io.ReadAll(fJson)
-
-	var langs []Compare
-	json.Unmarshal(byteValue, &langs)
-
-	randIndex := rand.Intn(len(langs))
-	choosenPart := langs[randIndex:(randIndex + countWords)]
-
-	// compare to finn words
-	type FinWords struct {
-		fact   string
-		byUser string
-	}
-	var finWords []FinWords
-
-	for _, word := range choosenPart {
-		fmt.Printf("Переведи слово на финский: %s\n", word.Eng)
-		text, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-
-		fmt.Printf("Введенное слово: %s\n", text)
-
-		var item FinWords
-		item.fact = strings.TrimSpace(word.Fin)
-		item.byUser = strings.TrimSpace(text)
-		finWords = append(finWords, item)
-	}
-
-	var countRights []string
-
-	for w := 0; w < len(finWords); w++ {
-		if finWords[w].fact == finWords[w].byUser {
-			countRights = append(countRights, finWords[w].byUser)
+		finW := r.FormValue("finword")
+		var ans string
+		if content[0].Fin == finW {
+			ans = "correct: " + content[0].Fin + " == " + finW
+		} else {
+			ans = "wrong: " + content[0].Fin + " != " + finW
 		}
+		w.Write([]byte(ans))
+	})
+
+	http.ListenAndServe(":80", nil)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	// get json data
+	f, err := os.Open("./source_data/qa.json")
+	if err != nil {
+		log.Println(err)
+	}
+	defer f.Close()
+	byteV, _ := io.ReadAll(f)
+	json.Unmarshal(byteV, &content)
+
+	var data Domdata
+	data.PageTitle = "Translate word to finnish"
+	data.EngValue = content[0].Eng
+	data.Answer = content[0].Fin
+
+	filename := "templates/index.html"
+	render(w, filename, data)
+}
+
+func render(w http.ResponseWriter, filename string, data interface{}) {
+	tmpl, err := template.ParseFiles(filename)
+	if err != nil {
+		log.Print(err)
+		http.Error(w, "Sorry, something went wrong", http.StatusInternalServerError)
+		return
 	}
 
-	fmt.Printf("Количество верных ответов: %d\n", len(countRights))
-	fmt.Printf("Правильные варианты: %v\n", countRights)
-	fmt.Printf("Процент правильных ответов: %.2f\n", float32(len(countRights))/float32(countWords))
+	if err := tmpl.Execute(w, data); err != nil {
+		log.Print(err)
+		http.Error(w, "Sorry, something went wrong", http.StatusInternalServerError)
+	}
 }
