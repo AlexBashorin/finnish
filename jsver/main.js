ac80f8f84e000aa58bb2a897c377eec18e6cc435
AlexBashorin <bashorina5@gmail.com>
2024-01-27 13:38:25 +0700

    correct fetch

--- jsver/main.js
+++ jsver/main.js
@@ -1,7 +1,7 @@
 // import data from "../source_data/qa.json" assert { type: 'json' };
 let data = []
 
-await fetch("https://github.com/AlexBashorin/finnish/tree/main/source_data/qa.json")
+await fetch("./source_data/qa.json")
 .then(response => {
    return response.json();
 })
